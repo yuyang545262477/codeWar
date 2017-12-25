a79fe7b6262208844d74b6bc633ecaa1f58b14cb
yuyang <yuyang545262477@gmail.com>
2017-12-25 10:11:34 +0800

    完成部分,并进行记录

--- simple/simple.js
+++ simple/simple.js
@@ -12,12 +12,53 @@ function simple(poly) {
             if (!regexTwo.test(item)) item = item.slice(0, 1) + 1 + item.slice(1).split("").sort().join("");
             return item;
         });
+    //TODO:计算前,进行排序
+    needSort();
     //TODO:对单项式进行运算
-    var readyArrayTwo = readyArray.map(function (value) {
-        return value.replace(/[^A-Za-z]/g, '');
-    });
-    console.log(readyArrayTwo);
-    console.log(readyArray);
-    //TODO:对计算结果,进行排序
+    var readyArrayTwo = computeArrayOne(readyArray);
+
+    // TODO:对计算结果,进行排序
+
     //TODO:整合计算结果
+    function computeArrayOne(readyArray) {
+        var backArray = [];
+
+        for (var i = 0; i < readyArray.length; i++) {
+            var polynomial = readyArray[i];
+            var _indexOfExit = getExitIndex(polynomial, backArray);
+            _indexOfExit !== -1 ? computeBackArray(polynomial) : pushBackArray(polynomial);
+        }
+
+        return backArray;
+
+        function getExitIndex(polynomial, backArray) {
+            //TODO:获取backArray 存在序列号
+            var coreItem = getCore(polynomial);
+            var _index = backArray.findIndex(function (item) {
+                return getCore(item) === coreItem;
+            });
+            return _index;
+        }
+
+        function pushBackArray(polynomial) {
+            //TODO:特殊处理的添加
+            var coreItem = getCore(polynomial);
+            if (/^[+]/.test(coreItem)) coreItem = coreItem.slice(1);
+            backArray.push(coreItem);
+        }
+
+        function computeBackArray(polynomial) {
+            //TODO:对存在的元素进行计算
+
+        }
+
+
+        function getCore(value) {
+            return value.replace(/[^A-Za-z]/g, '');
+        }
+
+
+    }
+
+
 }
