69f30289d4638c5c62057ac61f895d9e138f8fd3
yuyang <yuyang545262477@gmail.com>
2018-01-04 11:02:01 +0800

    finish romanEncode

--- 4kyu/RomanEncode/index.js
+++ 4kyu/RomanEncode/index.js
@@ -4,17 +4,24 @@ function solution(number) {
     var backString = '';
     var mapObj = {
         'M': 1000,
+        'CM': 900,
         'D': 500,
+        'CD': 400,
         'C': 100,
+        'XC': 90,
         'L': 50,
+        'XL': 40,
         'X': 10,
         'V': 5,
+        'IV': 4,
         'I': 1
     };
     Object.keys(mapObj).map(function (key) {
         var _number = mapObj[key];
-        if (number / _number > 0) backString = key;
-        else backString = solution(number % _number);
+        while (number >= _number) {
+            backString += key;
+            number -= _number;
+        }
     });
 
     return backString;
